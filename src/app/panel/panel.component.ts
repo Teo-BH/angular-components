28ad9fd3f0497016919e59f33ecb7fe96577c142
Teobaldo Moura <tmoura@ciandt.com>
2019-03-20 16:07:57 -0300

    Check translate answer

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -9,17 +9,41 @@ import { PHRASES } from './phrase.mock'
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { 
-    console.log(this.phrases)
+  constructor() {
+    this.currentPhrase = this.phrases[this.index] 
   }
 
   ngOnInit() { }
 
   public phrases: Phrase[] = PHRASES
   public title: string = "Traduza a expressão:"
+  public responseText: string
+
+  public index: number = 0
+  public currentPhrase: Phrase
 
   public updateText(sender: Event): void {
-    console.log((<HTMLInputElement>(sender.target)).value)
+    this.responseText = (<HTMLInputElement>(sender.target)).value
+  }
+
+  public checkText(): void {
+    if (this.currentPhrase.portuguesePhrase == this.responseText) {
+      alert('A tradução está correta')
+      this.nextPhrase()
+      this.responseText = ""
+    } else {
+      alert('A tradução está errada')
+  }
+    
+    console.log(this.index)
+  }
+
+  private nextPhrase() {
+    this.index++
+    if (this.index >= this.phrases.length) {
+      this.index = 0
+    }
+    this.currentPhrase = this.phrases[this.index] 
   }
   
 }
