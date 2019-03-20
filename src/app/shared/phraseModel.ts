b610718fb4e09697dc170e2a92f55696bbcd79be
Teobaldo Moura <tmoura@ciandt.com>
2019-03-20 11:37:43 -0300

    Add verbose phrase model

---
+++ src/app/shared/phraseModel.ts
@@ -0,0 +1,10 @@
+export class phraseModel {
+
+    public englishPhrase: string
+    public portuguesePhrase: string
+
+    constructor(englishPhrase: string, portuguesePhrase: string) {
+        this.englishPhrase = englishPhrase
+        this.portuguesePhrase = portuguesePhrase
+    }
+}
